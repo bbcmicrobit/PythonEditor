ba7d3c809d4c7ccc07632909b9c554477eb04381
Ross Lowe <30410608+microbit-rosslowe@users.noreply.github.com>
2019-03-19 15:49:42 +0000

    fs: Show error dialog when loading large module (#123)

--- python-main.js
+++ python-main.js
@@ -450,6 +450,10 @@ function web_editor(config) {
             return alert(config.translate.alerts.empty);
         }
         if (isModule) {
+            if (micropythonFs.getStorageRemaining() < 0){
+                micropythonFs.remove(filename);
+                return alert(config.translate.alerts.module_out_of_space);
+            }
             if (showModuleLoadedAlert) {
                 alert(config.translate.alerts.module_added.replace('{{module_name}}', moduleName));
             }
