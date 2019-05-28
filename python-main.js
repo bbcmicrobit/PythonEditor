59c2f60077da457683e15e4f78f2cb1ffda60f54
Daniel Baines <daniel.baines@arm.com>
2019-05-28 11:28:01 +0100

    Save/Load button merged + individual filesystem file downloading

--- python-main.js
+++ python-main.js
@@ -250,7 +250,7 @@ function web_editor(config) {
             EDITOR.focus();
         } else {
             // If there's no name, default to something sensible.
-            setName("microbit");
+            setName("my program");
             // If there's no description, default to something sensible.
             setDescription("A MicroPython script");
             // A sane default starting point for a new script.
@@ -500,16 +500,32 @@ function web_editor(config) {
         $('.fs-file-list table tbody').empty();
         micropythonFs.ls().forEach(function(filename) {
             var pseudoUniqueId = Math.random().toString(36).substr(2, 9);
-            // Check for main.py to exclude it from the table list
-            if (filename === 'main.py') return;
-            var fileType = (/[.]/.exec(filename)) ? /[^.]+$/.exec(filename) : "";
+            var name = filename;
+            var disabled = "";
+            if (filename === 'main.py') {
+              name = getName() + " (" + filename + ")";
+              disabled = "disabled";
+            };
             $('.fs-file-list table tbody').append(
-                '<tr><td>' + filename + '</td>' +
-                '<td>' + fileType + '</td>' +
+                '<tr><td>' + name + '</td>' +
                 '<td>' + (micropythonFs.size(filename)/1024).toFixed(2) + ' Kb</td>' +
-                '<td><button id="' + pseudoUniqueId + '" class="fs-remove-button">Remove</button></td></tr>'
+                '<td><button id="' + pseudoUniqueId + '2" class="save-button ' + disabled + '" title="Remove" style="width: 4em; height: 2em; float:right;"><i class="fa fa-trash"></i></button>' +
+                '<button id="' + pseudoUniqueId + '" class="save-button" title="Save" style="width: 4em; height: 2em; float:right;"><i class="fa fa-download"></i></button></td></tr>'
             );
             $('#' + pseudoUniqueId).click(function(e) {
+                console.log(filename);
+                var output = micropythonFs.readBytes(filename);
+                var ua = navigator.userAgent.toLowerCase();
+                if((ua.indexOf('safari/') > -1) && (ua.indexOf('chrome') == -1)) {
+                    alert(config.translate.alerts.save);
+                    window.open('data:application/octet;charset=utf-8,' + encodeURIComponent(output), '_newtab');
+                } else {
+                    var blob = new Blob([output], {type: "text/plain"});
+                    saveAs(blob, filename);
+                }
+            });
+            $('#' + pseudoUniqueId + '2').click(function(e) {
+                console.log(filename);
                 micropythonFs.remove(filename);
                 updateFileTables();
             });
@@ -517,7 +533,7 @@ function web_editor(config) {
         // Hide the table if it is empty
         var fileRowsInTable = $('#fs-file-list>table>tbody').has('tr').length;
         if (!fileRowsInTable) {
-            $('#fs-file-list>table').css('display', 'none');
+            //$('#fs-file-list>table').css('display', 'none');
         } else {
             $('#fs-file-list>table').css('display', '');
         }
@@ -565,28 +581,30 @@ function web_editor(config) {
         }
     }
 
-    // This function describes what to do when the save button is clicked.
-    function doSave() {
-        var output = EDITOR.getCode();
-        var ua = navigator.userAgent.toLowerCase();
-        if((ua.indexOf('safari/') > -1) && (ua.indexOf('chrome') == -1)) {
-            alert(config.translate.alerts.save);
-            window.open('data:application/octet;charset=utf-8,' + encodeURIComponent(output), '_newtab');
-        } else {
-            var filename = getName().replace(" ", "_");
-            var blob = new Blob([output], {type: "text/plain"});
-            saveAs(blob, filename + ".py");
-        }
-        dirty = false;
-    }
-
-    // Describes what to do when the load button is clicked.
-    function doLoad() {
+    // Describes what to do when the save/load button is clicked.
+    function doFiles() {
         var template = $('#load-template').html();
         Mustache.parse(template);
         vex.open({
             content: Mustache.render(template, config.translate.load),
             afterOpen: function(vexContent) {
+                $('#save-hex').click(function() {
+                    doDownload();
+                });
+                $('#show-files').click(function() {
+                  var content = $('.content')[0];
+                  if (content.style.maxHeight){
+                    content.style.maxHeight = null;
+                    $("#hide-files").attr("id", "show-files");
+                    $("#show-files").attr("title", "Show Files");
+                    document.getElementById("show-files").innerHTML = "Show Files <i class='fa fa-caret-down'>";
+                  } else {
+                    content.style.maxHeight = (content.scrollHeight + 200) + "px";
+                    $("#show-files").attr("id", "hide-files");
+                    $("#hide-files").attr("title", "Hide Files");
+                    document.getElementById("hide-files").innerHTML = "Hide Files <i class='fa fa-caret-up'>";
+                  }
+                });
                 $(vexContent).find('#load-drag-target').on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
                     e.preventDefault();
                     e.stopPropagation();
@@ -648,6 +666,9 @@ function web_editor(config) {
                         fileReader.readAsArrayBuffer(file);
                     });
                     inputFile.value = '';
+
+                    var content = $('.content')[0];
+                    content.style.maxHeight = (content.scrollHeight + 200) + "px";
                 });
             }
         });
@@ -818,11 +839,8 @@ function web_editor(config) {
         $("#command-download").click(function () {
             doDownload();
         });
-        $("#command-save").click(function () {
-            doSave();
-        });
-        $("#command-load").click(function () {
-            doLoad();
+        $("#command-files").click(function () {
+            doFiles();
         });
         $("#command-blockly").click(function () {
             doBlockly();
