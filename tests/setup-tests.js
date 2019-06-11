48ae588ebe1d58a9b437b6d8a05efdb062cded3d
Ross Lowe <30410608+microbit-rosslowe@users.noreply.github.com>
2019-06-11 16:07:59 +0100

    Update jasmine tests for autocompletion

--- tests/setup-tests.js
+++ tests/setup-tests.js
@@ -5,9 +5,9 @@
 require('../ace/ace.js');
 global.ace.edit = window.ace.edit;
 global.ace.require = window.ace.require;
-require("../ace/theme-kr_theme.js");
-require("../ace/mode-python");
-require("../ace/snippets/python.js");
+require("../ace/theme-kr_theme_legacy.js");
+require("../ace/mode-python_microbit");
+require("../ace/snippets/python_microbit.js");
 require('../ace/ext-language_tools.js');
 
 // Import the rest of the static libraries
