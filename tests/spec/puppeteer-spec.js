3dcdaf967f1b0b1ec45374b6e936c721b0e0781d
Ross Lowe <30410608+microbit-rosslowe@users.noreply.github.com>
2019-06-18 10:22:38 +0100

    Updates from feedback
    - rearrange AAA
    - remove dialogs from old hex tests
    - clarify flags
    - reduce dialog wait

--- tests/spec/puppeteer-spec.js
+++ tests/spec/puppeteer-spec.js
@@ -3,6 +3,8 @@ jest.setTimeout(20000);
 describe("An editor for MicroPython running at localhost.", function() {
 
     beforeAll(async() => {
+        // Setup a headless Chromium browser.
+        // Flags allow Puppeteer to run within a container.
         global.browser = await global.puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']});
     });
 
@@ -16,12 +18,13 @@ describe("An editor for MicroPython running at localhost.", function() {
         const projectURL = "http://localhost:5000/editor.html?#project:XQAAgAApAQAAAAAAAAA9gn0iDP5hOXUMBZ4M1sxt7nhTa/UMRecCSq6uHLM44uVEs1hTA1G/Oa3Hy9fjvqw9MOrrvyqKstR9g9oq4yc4pkk1m9E2hvucCCCVEeUdb6bwT0S5asuGStzirbKaXcmYjTAskliKk/1v60vUCxCI/fc8ZUstwqzchTG2zAzzDii/EzhUsce8bjtDMg+OOMAzY03WeyEN6x5Z3bkVA20HbuSfofyGzVIlKfTxKeZlZVU2Wt3DdOqe1ccGelN7y0dADIpV19vKoZ9AWI8K4l3FkQQ43EIIM/vyyq0+JjpgrLhtSv/8Ma+A";
         await page.goto(projectURL);
 
-        let hasLoadedContent = false;
         const codeContent = await page.evaluate("window.EDITOR.getCode();");
-        if (codeContent.includes("Test passed!") && codeContent.length === 111) hasLoadedContent = true;
+        const codeName = await page.evaluate("document.getElementById('script-name').innerHTML");
         await page.close();
 
-        expect(hasLoadedContent).toEqual(true);
+        expect(codeContent).toHaveLength(111);
+        expect(codeContent).toContain("Test passed!");
+        expect(codeName).toEqual("unearthly script 2");
 
     });
 
@@ -40,7 +43,7 @@ describe("An editor for MicroPython running at localhost.", function() {
         let fileInput = await page.$('[name="load-form-file-upload"]');
         await fileInput.uploadFile('./src/makecode.hex');
         await page.click('[value="Load"]');
-        await page.waitFor(2000);
+        await page.waitFor(1000);
         await page.close();
 
         expect(hasShownError).toEqual(true);
@@ -52,22 +55,18 @@ describe("An editor for MicroPython running at localhost.", function() {
         const page = await global.browser.newPage();
         await page.goto("http://localhost:5000/editor.html");
 
-        let hasLoadedV101Hex = false;
-        page.on('dialog', async dialog => {
-            await dialog.accept();
-            hasLoadedV101Hex = false;
-        });
         await page.click('#command-load');
         await page.click('.load-drag-target.load-toggle');
         let fileInput = await page.$('[name="load-form-file-upload"]');
         await fileInput.uploadFile('./src/1.0.1.hex');
         await page.click('[value="Load"]');
         const codeContent = await page.evaluate("window.EDITOR.getCode();");
-        if (codeContent.includes("PASS1") && codeContent.length === 32) hasLoadedV101Hex = true;
-        await page.waitFor(2000);
+        const codeName = await page.evaluate("document.getElementById('script-name').innerHTML");
         await page.close();
 
-        expect(hasLoadedV101Hex).toEqual(true);
+        expect(codeContent).toHaveLength(32);
+        expect(codeContent).toContain("PASS1");
+        expect(codeName).toEqual("1.0.1");
 
     });
 
@@ -76,22 +75,18 @@ describe("An editor for MicroPython running at localhost.", function() {
         const page = await global.browser.newPage();
         await page.goto("http://localhost:5000/editor.html");
 
-        let hasLoadedV09Hex = false;
-        page.on('dialog', async dialog => {
-            await dialog.accept();
-            hasLoadedV09Hex = false;
-        });
         await page.click('#command-load');
         await page.click('.load-drag-target.load-toggle');
         let fileInput = await page.$('[name="load-form-file-upload"]');
         await fileInput.uploadFile('./src/0.9.hex');
         await page.click('[value="Load"]');
         const codeContent = await page.evaluate("window.EDITOR.getCode();");
-        if (codeContent.includes("PASS2") && codeContent.length === 31) hasLoadedV09Hex = true;
-        await page.waitFor(2000);
+        const codeName = await page.evaluate("document.getElementById('script-name').innerHTML");
         await page.close();
 
-        expect(hasLoadedV09Hex).toEqual(true);
+        expect(codeContent).toHaveLength(31);
+        expect(codeContent).toContain("PASS2");
+        expect(codeName).toEqual("0.9");
 
     });
 
