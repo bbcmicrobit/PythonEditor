21d41db17ce469614b7d5d11c36debabd5c406c5
Jakub Lenski <Jakub.Lenski@arm.com>
2019-07-12 13:00:19 +0100

    Added more jest tests, improved ES5 support, adressed PR comments

--- tests/spec/urlparser-spec.js
+++ tests/spec/urlparser-spec.js
@@ -3,13 +3,13 @@
  */
 
 
-let defaultUrl = "https://python.microbit.org/v/1.1"; 
+var defaultUrl = "https://python.microbit.org/v/1.1"; 
 describe("Testing different URL formats", () => {
     test("Check if no Parameter is recognised", () =>{
-        expect(global.urlparse("parameter", defaultUrl) === undefined).toBeTruthy();
+        expect(global.urlparse("parameter", defaultUrl)).toBeUndefined();
     });
     test("Check if URL fragments are ignored", ()=>{
-        expect(global.urlparse("anchor", defaultUrl + "#anchor") === undefined).toBeTruthy();
+        expect(global.urlparse("anchor", defaultUrl + "#anchor")).toBeUndefined();
     });
     test("Check if parameter is recognised", () =>{
         expect(global.urlparse("key", defaultUrl + "?key=value1") === "value1").toBeTruthy();
@@ -24,15 +24,37 @@ describe("Testing different URL formats", () => {
         expect(global.urlparse("key2", defaultUrl + "?key1&key2#anchor") === null).toBeTruthy();
     });
     test("Check if incomplete URL parameter returns null", ()=>{
-        expect(global.urlparse("anchor", defaultUrl + "?key1=") === null).toBeTruthy();
+        expect(global.urlparse("key1", defaultUrl + "?key1=")).toBeNull();
     });
     test("Check if a URL parameter followed by an ampersand returns correct value", ()=>{
         expect(global.urlparse("key1", defaultUrl + "?key1=value&")).toBe("value");
     });
     test("Check if an incomplete URL parameter followed by an ampersand returns undefined", ()=>{
-        expect(global.urlparse("key1", defaultUrl + "?key1=&")).toBe(null);
+        expect(global.urlparse("key1", defaultUrl + "?key1=&")).toBeNull();
     });
     test("Check if an incomplete URL parameter followed by an ampersand and a URL fragment returns undefined", ()=>{
-        expect(global.urlparse("key1", defaultUrl + "?key1=&#anchor")).toBe(null);
+        expect(global.urlparse("key1", defaultUrl + "?key1=&#anchor")).toBeNull();
     });
-})
+    test("Check if a URL parameter value of 0 returns string '0'", ()=>{
+        expect(global.urlparse("key1", defaultUrl + "?key1=0")).toBe("0");
+    });
+    test("Check if a URL parameter value of null returns string 'null'", ()=>{
+        expect(global.urlparse("key1", defaultUrl + "?key1=null")).toBe("null");
+    });
+    test("Check if a URL parameter value of undefined returns string 'undefined'", ()=>{
+        expect(global.urlparse("key1", defaultUrl + "?key1=undefined")).toBe("undefined");
+    });
+    test("Check if incomplete URL parameter with url fragment returns null", ()=>{
+        expect(global.urlparse("key1", defaultUrl + "?key1=#anchor")).toBeNull();
+    });
+    test("Check if same key set twice returns last value", ()=>{
+        expect(global.urlparse("key1", defaultUrl + "?key1=value1&key1=value2")).toBe("value2");
+    });
+    test("Check if both url parameter keys return the correct value", ()=>{
+        expect(global.urlparse("key1", defaultUrl + "?key1=value1&key2=value2") === "value1" && 
+        global.urlparse("key2", defaultUrl + "?key1=value1&key2=value2") === "value2").toBeTruthy();
+    });
+    test("Check if a non-existent key with existing key returns undefined", ()=>{
+        expect(global.urlparse("notpresent", defaultUrl + "?key1=value1")).toBeUndefined();
+    });
+});
--- urlparser.js
+++ urlparser.js
@@ -1,7 +1,25 @@
+/**
+ * 
+ * function searches the url parameters to find the specified key
+ * before returning the value associated with that key in string form.
+ * 
+ * If the key is found but no value is assigned (e.g. ?key1=) the value
+ * returned will be null
+ * 
+ * If the key is not found the value returned will be undefined.
+ * 
+ * @param {string} key Specifies the key to search for in the Url parameters  
+ * @param {string} url Specifies custom url to search for the key for (implemented for testing)
+ * 
+ * If key is found and value exists, returns string
+ * If key is found and value does not exist, returns null
+ * If no key is found, returns undefined
+ */
+
 var urlparse = 
     function getUrlParam(key, url) {
         if (url === undefined) {
-            url = window.location.search;
+            url = window.location.href;
         }
         var urlParams = url.split("?")[1];
         if (urlParams === undefined) {
@@ -11,10 +29,10 @@ var urlparse =
             //Removes anchor/url fragments from the end of the url
            urlParams = urlParams.slice(0,urlParams.indexOf('#'));
         }
-        var value = null;
+        var value = undefined;
         urlParams = urlParams.split("&");
         urlParams.forEach(function (param) {
-            let parameter = {
+            var parameter = {
                 key: param.split("=")[0],
                 value: param.split("=")[1]
             }
