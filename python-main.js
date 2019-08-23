5994349def1c8d930b9644f7b4fe2297740b8422
microbit-carlos <carlos@microbit.org>
2019-08-23 13:27:01 +0100

    Merging setLang and updateLang.

--- python-main.js
+++ python-main.js
@@ -268,31 +268,33 @@ function blocks() {
  */
 function translations() {
     'use strict';
-    var validLangs = ['en','es','pl'];
+    // These values must be valid language codes
+    // https://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+    var validLangs = ['en', 'es', 'pl'];
 
     /* Replaces DOM script element with the new language js file. */
     function updateLang(newLang, callback) {
         var elementId = 'lang';
-        document.getElementById(elementId).remove();
-        script('lang/' + newLang + '.js', elementId);
-        document.getElementById(elementId).onload = function() {
+        var newLangURL = 'lang/' + newLang + '.js';
+        var endsWithURL = new RegExp(newLangURL + "$");
+        var runCallback = function() {
             translateEmbedStrings(language);
             callback(language);
         };
-    }
-
-    /* Checks URL for query parameter 'l' and translates page to that language. */
-    function setLang(message){
-        if (message.l){
-            var lang = message.l;
-            if (validLangs.indexOf(lang)>-1){
-                updateLang(lang, function(translations) {
-                    config.translate = translations;
-                    document.getElementsByTagName("HTML")[0].setAttribute("lang", lang);
-                });
-            }else{
-            }
-            }
+        if (endsWithURL.test(document.getElementById(elementId).src)) {
+            // The request newLang is the current one, don't reload js file
+            return runCallback(language);
+        }
+        // Check for a valid language
+        if (validLangs.indexOf(newLang) >- 1) {
+            document.getElementById(elementId).remove();
+            script(newLangURL, elementId);
+            document.getElementById(elementId).onload = runCallback;
+        } else {
+            // Don't throw an error, but inform the console
+            runCallback();
+            console.error('Requested language not available: ' + newLang);
+        }
     }
 
     /* Replaces the strings already loaded in the DOM, the rest are dynamically loaded. */
@@ -327,9 +329,7 @@ function translations() {
     }
 
     return {
-        'updateLang': updateLang,
-        'translateEmbedStrings': translateEmbedStrings,
-        'setLang': setLang,
+        'updateLang': updateLang
     };
 }
 
@@ -422,6 +422,13 @@ function web_editor(config) {
         if (continueZooming) {
             BLOCKS.zoomOut();
         }
+    }
+
+    function setLanguage(lang) {
+        TRANSLATIONS.updateLang(lang, function(translations) {
+            config.translate = translations;
+            document.getElementsByTagName("HTML")[0].setAttribute("lang", lang);
+        });
     }
 
     // Checks for feature flags in the config object and shows/hides UI
@@ -1375,10 +1382,7 @@ function web_editor(config) {
 
         $(".lang-choice").on("click", function() {
             $("#language_container").addClass('hidden');
-            TRANSLATIONS.updateLang($(this).attr('id'), function(translations) {
-                config.translate = translations;
-                document.getElementsByTagName("HTML")[0].setAttribute("lang", lang);
-            });
+            setLanguage($(this).attr('id'));
         });
 
         $('#menu-switch-autocomplete').on('change', function() {
@@ -1442,8 +1446,7 @@ function web_editor(config) {
     setupFeatureFlags();
     setupEditor(qs, migration);
     setupButtons();
-    TRANSLATIONS.translateEmbedStrings(config.translate);
-    TRANSLATIONS.setLang(qs);
+    setLanguage(qs.l || 'en');
     document.addEventListener('DOMContentLoaded', function() {
         // Firmware at the end of the HTML file has to be loaded first
         setupFilesystem();
