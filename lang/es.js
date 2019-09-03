191d07edd3e7b7d6ad13049f92927b63d479a596
Carlos Pereira Atencio <carlos@microbit.org>
2019-09-03 15:50:30 +0100

    Lang: Finish Spanish translations. (#230)

--- lang/es.js
+++ lang/es.js
@@ -1,50 +1,50 @@
 var language = {
   'code_snippets': {
     'title': 'Fragmentos de Codigo',
-    'description': "Code snippets are short blocks of code to re-use in your own programs. There are snippets for most common things you'll want to do using MicroPython.",
-    'instructions': "Select one of the snippets below, or type the snippet's trigger then tap the TAB key.",
-    'trigger_heading': 'trigger',
-    'description_heading': 'description',
-    'docs': 'create a comment to describe your code',
-    'wh': 'while some condition is True, keep looping over some code',
-    'with': 'do some stuff with something assigned to a name',
-    'cl': 'create a new class that defines the behaviour of a new type of object',
-    'def': 'define a named function that takes some arguments and optionally add a description',
-    'if': 'if some condition is True, do something',
-    'ei': 'else if some other condition is True, do something',
-    'el': 'else do some other thing',
-    'for': 'for each item in a collection of items do something with each item',
-    'try': 'try doing something and handle exceptions (errors)'
+    'description': "Los fragmentos de código son bloques cortos de código para reutilizar en tus programas. Hay fragmentos de las cosas más comunes que querrás hacer con MicroPython.",
+    'instructions': "Selecciona uno de los fragmentos o escribe el atajo del fragmento y presiona la tecla TAB.",
+    'trigger_heading': 'atajo',
+    'description_heading': 'descripción',
+    'docs': 'crea un comentario para describir tu código',
+    'wh': 'mientras que alguna condición es verdadera, continua ejecutando el código',
+    'with': 'haz algo con algo asignado a un nombre',
+    'cl': 'crea una nueva clase que define el comportamiento de un nuevo tipo de objeto',
+    'def': 'define una función con nombre propio que toma algunos argumentos y opcionalmente añade una descripción',
+    'if': 'si alguna condición es verdadera, haz algo',
+    'ei': 'de lo contrario, si alguna otra condición es verdadera, haz algo',
+    'el': 'de lo contratio, haz otra cosa',
+    'for': 'por cada elemento de una colección de elementos, haz algo con cada elemento',
+    'try': 'intenta hacer algo y maneja las excepciones (errores)'
   },
   'share': {
-    'title': 'Share Code',
-    'instructions': 'Use a password and optional hint (to help you remember the password) to securely create a link to share your code with others.',
-    'passphrase': 'Password: ',
-    'hint': 'Password hint: ',
-    'button': 'Create Link',
-    'description': 'This URL points to your code:',
-    'shortener': 'This is a short version of the link:'
+    'title': 'Compartir código',
+    'instructions': 'Use una contraseña y una sugerencia opcional (para ayudarte a recordar la contraseña) para crear de forma segura un enlace para compartir tu código con otros.',
+    'passphrase': 'Contraseña: ',
+    'hint': 'Pista de la contraseña: ',
+    'button': 'Crear enlace',
+    'description': 'Esta URL apunta a tu código:',
+    'shortener': 'Esta es una versión corta del enlace:'
   },
   'decrypt': {
-    'title': 'Decrypt Source Code',
-    'instructions': 'Enter a password to decrypt the source code.',
-    'passphrase': 'Password: ',
-    'button': 'Decrypt'
+    'title': 'Descifrar el Código Fuente',
+    'instructions': 'Entra una contraseña para descifrar el código fuente.',
+    'passphrase': 'Contraseña: ',
+    'button': 'Descifrar'
   },
   'alerts': {
-    'download': 'Safari has a bug that means your work will be downloaded as an un-named file. Please rename it to something ending in .hex. Alternatively, use a browser such as Firefox or Chrome. They do not suffer from this bug.',
-    'save': 'Safari has a bug that means your work will be downloaded as an un-named file. Please rename it to something ending in .py. Alternatively, use a browser such as Firefox or Chrome. They do not suffer from this bug.',
-    'load_code': 'Oops! Could not load the code into the hex file.',
-    'unrecognised_hex': "Sorry, we couldn't recognise this file",
-    'snippets': 'Snippets are disabled when blockly is enabled.',
+    'download': 'Safari tiene un error que significa que el trabajo se descargará como un archivo sin nombre. Por favor, cambia el nombre a algo que termine en .hex. Alternativamente, usa un navegador como Firefox o Chrome. No sufren de este error.',
+    'save': 'Safari tiene un error que significa que el trabajo se descargará como un archivo sin nombre. Por favor, cambia el nombre a algo que termine en .py. Alternativamente, usa un navegador como Firefox o Chrome. No sufren de este error.',
+    'load_code': '¡Uy! No se pudo añadir el código en el archivo hex.',
+    'unrecognised_hex': "Lo sentimos, no pudimos reconocer este archivo",
+    'snippets': 'Los fragmentos están deshabilitados cuando blockly está habilitado.',
     'error': 'Error:\n',
-    'empty': 'The Python file does not have any content.',
-    'no_python': 'Could not find valid Python code in the hex file.',
-    'no_script': 'Hex file does not contain a Python script.',
-    'no_main': 'The hex file does not contain a main.py file.',
-    'cant_add_file': 'Could not add file to the system:',
-    'module_added': 'The "{{module_name}}" module has been added to the filesystem.',
-    'module_out_of_space': 'Could not add file to the system as there is no storage space left.'
+    'empty': 'El archivo Python no tiene ningún contenido.',
+    'no_python': 'No se pudo encontrar código válido de Python en el archivo hex.',
+    'no_script': 'El archivo hex no contiene un script de Python.',
+    'no_main': 'El archivo hex no contiene un archivo main.py.',
+    'cant_add_file': 'No se pudo añadir el archivo al sistema de archivos:',
+    'module_added': 'El módulo "{{module_name}}" se ha agregado al sistema de archivos.',
+    'module_out_of_space': 'No se pudo añadir el archivo al sistema ya que no queda espacio de almacenamiento.'
   },
   'help': {
     'docs-link': {
@@ -71,12 +71,12 @@ var language = {
     'mp-ver': 'Versión de MicroPython:'
   },
   'confirms': {
-    'quit': "Some of your changes have not been saved. Quit anyway?",
-    'blocks': "You have unsaved code. Using blocks will change your code. You may lose your changes. Do you want to continue?",
-    'replace_main': 'Adding a main.py file will replace the code in the editor!',
-    'replace_file': 'Do you want to replace the "{{file_name}}" file?',
-    'replace_module': 'Do you want to replace the "{{module_name}}" module?',
-    'download_py_multiple': 'This project contains multiple files that will not be saved using this format.\nWe recommend downloading the Hex file, which contains your entire project and can be loaded back into the editor.\n\n Are you sure you want to download the {{file_name}} file only?'
+    'quit': "Algunos de sus cambios no se han guardado. Salir de todos modos?",
+    'blocks': "Tienes un código sin guardar. Usar los bloques cambiará tu código. Puede perder tus cambios. ¿Quieres continuar?",
+    'replace_main': '¡Agregar un archivo main.py reemplazará el código en el editor!',
+    'replace_file': '¿Quieres reemplazar el archivo "{{file_name}}"?',
+    'replace_module': '¿Quieres reemplazar el módulo "{{module_name}}"?',
+    'download_py_multiple': 'Este proyecto contiene varios archivos que no se guardarán con este formato.\nRecomendamos descargar el archivo Hex, que contiene todo el proyecto y puede volver a cargarse en el editor.\n\n¿Estas seguro que quieres descargar el {{file_name}} archivo solo?'
   },
   'code': {
     'start': "# Add your Python code here. E.g.\n" +
