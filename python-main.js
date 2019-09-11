d092b31ff78b57678dd993c26a107686c8f88839
microbit-josephine <54458430+microbit-josephine@users.noreply.github.com>
2019-09-11 13:14:38 +0100

    Disable autocomplete on enter

--- python-main.js
+++ python-main.js
@@ -455,6 +455,7 @@ function web_editor(config) {
             EDITOR.ACE.renderer.scroller.style.backgroundImage = "url('static/img/experimental.png')";
             EDITOR.enableAutocomplete(true);
             $('#menu-switch-autocomplete').prop("checked", true);
+            $('#menu-switch-autocomplete-on-enter').prop("checked", false);
         }
 
         // Update the help link to pass feature flag information.
