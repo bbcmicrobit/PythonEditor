bd411b0b5cfa68bfd73418df89062ccec306b36b
Sam Kent <sam@microbit.org>
2019-10-02 15:29:04 +0100

    Enable console.log for partial flashing

--- partial-flashing.js
+++ partial-flashing.js
@@ -38,8 +38,7 @@
 let PartialFlashingUtils = {
     pageSize: 1024,
     numPages: 256,
-    log: function() {},
-    // log: console.log,
+    log: console.log,
 
     // The Control/Status Word register is used to configure and control transfers through the APB interface.
     // This is drawn from https://github.com/mmoskal/dapjs/blob/a32f11f54e9e76a9c61896ddd425c1cb1a29c143/src/dap/constants.ts#L28
