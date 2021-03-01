d6733fb81bf44cd8b6e5df1080efd62cd7430873
microbit-carlos <carlos@microbit.org>
2021-03-01 14:26:19 +0000

    Tests: Improve test flackiness.

--- tests/spec/puppeteer-fs-spec.js
+++ tests/spec/puppeteer-fs-spec.js
@@ -2,8 +2,6 @@ const fs = require("fs");
 const tmp = require("tmp");
 const puppeteer = require("puppeteer");
 
-jest.setTimeout(60000 * 5);
-
 describe("Puppeteer filesystem tests for the Python Editor.", function() {
     "use strict";
 
@@ -23,9 +21,29 @@ describe("Puppeteer filesystem tests for the Python Editor.", function() {
         browser.close();
     });
 
-    it("Can store the correct number of small files in the filesystem", async function() {
+    async function getEditorPage() {
         const page = await browser.newPage();
+        let fsLoaded = false;
+        page.on('console', (msg) => {
+            const msgTxt = msg.text();
+            if (msgTxt == 'FS fully initialised') {
+                fsLoaded = true;
+            } else {
+                console.log('PAGE LOG:', msgTxt);
+            }
+        });
         await page.goto("http://localhost:5000/editor.html");
+        while (!fsLoaded) {
+            await page.waitForTimeout(5);
+        }
+        // The following two lines enable CPU throttling
+        // const client = await page.target().createCDPSession();
+        // await client.send('Emulation.setCPUThrottlingRate', { rate: 4 });
+        return page;
+    }
+
+    it("Can store the correct number of small files in the filesystem", async function() {
+        const page = await getEditorPage();
         // We expect to be able to add 160 small files to the fs (ignoring main.py)
         const expectedFileLimit = 159;
         let fileNameList = [];
@@ -41,18 +59,22 @@ describe("Puppeteer filesystem tests for the Python Editor.", function() {
             fileNameList.push(tmpFile.name);
             fileCleanUps.push(tmpFile.removeCallback);
         }
-        console.log("> Uploading files (this may take some time)...");
-        await fileInput.uploadFile(...fileNameList);
+        // Set up a listener for the dialog that will tell us fs is full
         let fileLimit = 0;
         page.on("dialog", async dialog => {
             await dialog.accept();
             if (dialog.message().includes("There is no storage space left.")) {
-                // Get the number of files (the error warns that we can't add the 215th file, but we expect 215 files as there are 214 small.py files + main.py)
+                // Get the number of files (the error warns that we can't add the 215th file, 
+                // but we expect 215 files as there are 214 small.py files + main.py)
                 fileLimit = parseInt(dialog.message().split("small")[1].split("-")[0]);
+            } else {
+                console.error("Unexpected dialog msg:", dialog.message());
             }
         });
-        for (let ms = 0; ms < (60000 * 3); ms += msStep) {
-            if (fileLimit !== 0) break;
+        console.log("> Uploading files (this may take some time)...");
+        await fileInput.uploadFile(...fileNameList);
+        // fileLimit changes on the dialog above, if it doesn't jest times out
+        while (fileLimit === 0) {
             await page.waitForTimeout(msStep);
         }
         await page.close();
@@ -61,26 +83,27 @@ describe("Puppeteer filesystem tests for the Python Editor.", function() {
         });
 
         expect(fileLimit).toEqual(expectedFileLimit);
-    });
+    }, 10 * 60 * 1000);
 
     it("Can store one large file in the filesystem", async function() {
-        const page = await browser.newPage();
-        await page.goto("http://localhost:5000/editor.html");
+        const page = await getEditorPage();
         const initialCode = await page.evaluate("window.EDITOR.getCode();");
         let codeContent = "";
-        let noErrorOnDownload = true;
+        let errorOnDownload = false;
 
         await page.click("#command-files");
         const fileInput = await page.$("#file-upload-input");
         await fileInput.uploadFile("./spec/test-files/large-20k.py");
-        for (let ms = 0; ms < 1000; ms += msStep) {
+        for (let ms = 0; ms < 4000; ms += msStep) {
             codeContent = await page.evaluate("window.EDITOR.getCode();");
             if (codeContent != initialCode) break;
             await page.waitForTimeout(msStep);
         }
         page.on("dialog", async dialog => {
             if (dialog.message().includes("enough space")) {
-                noErrorOnDownload = false;
+                errorOnDownload = true;
+            } else {
+                console.error("Unexpected dialog message received:", dialog.message());
             }
             await dialog.accept();
         });
@@ -93,12 +116,11 @@ describe("Puppeteer filesystem tests for the Python Editor.", function() {
         expect(codeContent).toHaveLength(20141);
         expect(codeContent).toContain("import love");
         expect(codeName).toEqual("large-20k");
-        expect(noErrorOnDownload).toEqual(true);
-    });
+        expect(errorOnDownload).toBeFalsy();
+    }, 30 * 1000);
 
     it("Shows an error when loading a file to the filesystem that is too large", async function() {
-        const page = await browser.newPage();
-        await page.goto("http://localhost:5000/editor.html");
+        const page = await getEditorPage();
         const initialCode = await page.evaluate("window.EDITOR.getCode();");
         const initialName = await page.evaluate("document.getElementById('script-name').value");
         let codeContent = "";
@@ -113,7 +135,7 @@ describe("Puppeteer filesystem tests for the Python Editor.", function() {
         await page.click("#command-files");
         const fileInput = await page.$("#fs-file-upload-input");
         await fileInput.uploadFile("./spec/test-files/too-large.py");
-        for (let ms=0; ms<100; ms++) {
+        for (let ms=0; ms < 1000; ms++) {
             if (rejectedLargeFileLoad) break;
             await page.waitForTimeout(10);
         }
@@ -126,11 +148,10 @@ describe("Puppeteer filesystem tests for the Python Editor.", function() {
         expect(codeContent).not.toContain("# Filler");
         expect(codeName).toEqual(initialName);
         expect(codeName).not.toEqual("main");
-    });
+    }, 60 * 1000);
 
     it("Correctly loads files via the load modal", async function() {
-        const page = await browser.newPage();
-        await page.goto("http://localhost:5000/editor.html");
+        const page = await getEditorPage();
         const initialCode = await page.evaluate("window.EDITOR.getCode();");
         let mainPyWarningDialogShows = false;
         let codeOnCancel = "";
@@ -146,20 +167,21 @@ describe("Puppeteer filesystem tests for the Python Editor.", function() {
                 if (!hasAttemptedCancel){
                     // Ensure that pressing 'Cancel' doesn't replace code contents
                     await dialog.dismiss();
+                    await page.waitForTimeout(10);
                     codeOnCancel = await page.evaluate("window.EDITOR.getCode();");
                     hasAttemptedCancel = true;
                     await fileInput.uploadFile("./spec/test-files/main.py");
                 } else {
                     // Ensure that pressing 'Accept' does replace code contents
                     await dialog.accept();
+                    await page.waitForTimeout(10);
                     codeOnAccept = await page.evaluate("window.EDITOR.getCode();");
                     fileListContents = await page.evaluate("$('#fs-file-list').html()");
                 }
             }
         });
         await fileInput.uploadFile("./spec/test-files/main.py");
-        for (let ms = 0; ms < 1000; ms += msStep) {
-            if (fileListContents !== "") break;
+        while (fileListContents == "") {
             await page.waitForTimeout(msStep);
         }
         await page.close();
@@ -169,11 +191,10 @@ describe("Puppeteer filesystem tests for the Python Editor.", function() {
         expect(codeOnAccept).not.toEqual(initialCode);
         expect(codeOnAccept).toContain("PASS");
         expect(fileListContents).toContain("main.py");
-    });
+    }, 60 * 1000);
 
     it("Correctly imports modules with the 'magic comment' in the filesystem.", async function() {
-        const page = await browser.newPage();
-        await page.goto("http://localhost:5000/editor.html");
+        const page = await getEditorPage();
         const initialContent = await page.evaluate("window.EDITOR.getCode();");
         let fileInput = undefined;
         page.on("dialog", async dialog => {
@@ -223,16 +244,21 @@ describe("Puppeteer filesystem tests for the Python Editor.", function() {
         expect(magicFourthlineName).toEqual("fourthline");
         expect(magicFourthlineContent).toContain("PASS");
         expect(magicFourthlineContent).toHaveLength(136);
-    });
+    }, 30 * 1000);
 
     it("Correctly loads a python script with the correct name", async function(){
-        const page = await browser.newPage();
-        await page.goto("http://localhost:5000/editor.html");
+        const page = await getEditorPage();
         await page.click("#command-files");
+        const startFileName = await page.evaluate("document.getElementById('script-name').value");
         let fileInput = await page.$("#file-upload-input");
+
         await fileInput.uploadFile("./spec/test-files/samplefile.py");
-        await page.waitForTimeout(500);
-        const fileName = await page.evaluate("document.getElementById('script-name').value");
+        let fileName = await page.evaluate("document.getElementById('script-name').value");
+        while (fileName === startFileName) {
+            await page.waitForTimeout(msStep);
+            fileName = await page.evaluate("document.getElementById('script-name').value");
+        }
+
         expect(fileName).toContain("samplefile");
-    });
+    }, 60 * 1000);
 });
